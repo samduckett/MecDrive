bd323def9e16293c6ee7022aac1b78e447d65eaa
Sam Duckett <sduckett@dawsonstudents.org>
2023-11-08 23:58:35 -0700

    Fixxing error

--- src/main/java/frc/lib/util/XboxControllerClass.java
+++ src/main/java/frc/lib/util/XboxControllerClass.java
@@ -1,6 +1,5 @@
 package frc.lib.util;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
